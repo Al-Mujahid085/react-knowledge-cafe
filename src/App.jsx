ca59deba4f2eb0a7405178c323809deb8d52663d
Al Mujahid <amuzahid085@gmail.com>
2024-11-04 17:57:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Blogs from './components/blogs/Blogs'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
